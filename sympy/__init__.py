41cdb02e4ccd73f9320eb424de10864c8c29f88e
mhon2014 <mhon2014@my.fit.edu>
2018-12-12 18:01:52 -0500

    Update __init__.py

--- sympy/__init__.py
+++ sympy/__init__.py
@@ -5,6 +5,8 @@ as possible in order to be comprehensible and easily extensible.  SymPy is
 written entirely in Python. It depends on mpmath, and other external libraries
 may be optionally for things like plotting support.
 
+This branch is being worked on by MaltNCigars
+
 See the webpage for more information and documentation:
 
     https://sympy.org
